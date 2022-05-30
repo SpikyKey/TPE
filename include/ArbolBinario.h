7d15dc357100b89e3ebcddff50d22a6b5baa95cc
Diego <moriego71@gmail.com>
2022-05-30 17:56:55 -0300

    Implementacion Arbol Binario

--- include/ArbolBinario.h
+++ include/ArbolBinario.h
@@ -1,16 +1,40 @@
-#ifndef ARBOLBINARIO_H
-#define ARBOLBINARIO_H
+#ifndef Arbin_H
+#define Arbin_H
 
-
-class ArbolBinario
+#include "../include/Lista.h"
+template <typename T> class Arbin
 {
     public:
-        ArbolBinario();
-        virtual ~ArbolBinario();
+        Arbin();
+        virtual ~Arbin();
+        void cargar(T elemento);
+	    bool pertenece(T elemento);
+        unsigned int cantelmentos();
+	    bool vacio ();
+	    unsigned int profundidad ();
+	    Lista listarFrontera ();
+	    Lista listarArbin();
+
+
 
     protected:
 
     private:
+        typedef struct NodoArbin{
+            T campo;
+            struct NodoArbin * hijoizq;
+            struct NodoArbin * hijoder;
+        }NodoArbin;
+
+        NodoArbin* raiz;
+        void cargarRecursivo(NodoArbin*& raiz, T elemento);
+        bool perteneceRecursiva(NodoArbin* raiz, T elemento);
+        unsigned int contarRecursivo(NodoArbin* raiz);
+        unsigned int profundidadRecursiva(NodoArbin* raiz);
+        Lista listarFronteraRecursiva(NodoArbin* raiz, Lista Frontera);
+        void listarRecursivo(NodoArbin* raiz, Lista & lista);
+        void destructorRecursivo(NodoArbin* raiz);
+
 };
 
 #endif // ARBOLBINARIO_H
--- src/ArbolBinario.cpp
+++ src/ArbolBinario.cpp
@@ -1,11 +1,143 @@
 #include "../include/ArbolBinario.h"
+#include "../include/Lista.h"
+#include <stdlib.h>
+#include <assert.h>
+#include <iostream>
 
-ArbolBinario::ArbolBinario()
+template <typename T>
+Arbin<T>::Arbin()
 {
-    //ctor
+    raiz = NULL;
 }
 
-ArbolBinario::~ArbolBinario()
+
+template <typename T>
+void Arbin<T>::cargarRecursivo(NodoArbin*& raiz, T elemento){
+    if (raiz == NULL){
+        NodoArbin* nuevo = new NodoArbin;
+        nuevo->campo = elemento;
+        nuevo->hijoizq = NULL;
+        nuevo->hijoder = NULL;
+        raiz = nuevo;
+        return;
+    }
+    if (elemento < raiz->elemento){
+        raiz->hijoizq = cargarRecursivo(raiz->hijoizq, elemento);
+    } else {
+        raiz->hijoder = cargarRecursivo(raiz->hijoder, elemento);
+    }
+    return;
+}
+template <typename T>
+void Arbin<T>::cargar(T elemento){
+    raiz = cargarRecursivo(raiz, elemento);
+}
+
+template <typename T>
+bool Arbin<T>::perteneceRecursiva(NodoArbin* raiz, T elemento){
+    if (raiz == NULL)
+        return false;
+
+    if (elemento == raiz->campo)
+        return true;
+
+    if (elemento < raiz->campo)
+        return perteneceRecursiva(raiz->hijoizq, elemento);
+    return perteneceRecursiva(raiz->hijoder, elemento);
+}
+
+template <typename T>
+bool Arbin<T>::pertenece(T elemento){
+    return perteneceRecursiva(raiz, elemento);
+}
+
+template <typename T>
+bool Arbin<T>::vacio(){
+    return raiz == NULL;
+ }
+
+ template <typename T>
+ unsigned int Arbin<T>::contarRecursivo(NodoArbin* raiz){
+    if (raiz == NULL)
+        return 0;
+    return 1 + contarRecursivo(raiz->hijoizq) +
+               contarRecursivo(raiz->hijoder);
+ }
+ template <typename T>
+ unsigned int  Arbin<T>::cantelmentos(){
+    return contarRecursivo(raiz);
+ }
+
+template <typename T>
+unsigned int Arbin<T>::profundidadRecursiva(NodoArbin* raiz){
+    if (raiz == NULL)
+        return 0;
+
+    unsigned int profundidadIzq;
+    unsigned int profundidadDer;
+    unsigned int mayor;
+    profundidadIzq = profundidadRecursiva(raiz->hijoizq);
+    profundidadDer = profundidadRecursiva(raiz->hijoder);
+    if (profundidadIzq < profundidadDer)
+        mayor = profundidadDer;
+    else
+        mayor = profundidadIzq;
+    return mayor + 1;
+}
+template <typename T>
+unsigned int Arbin<T>::profundidad (){
+    return profundidadRecursiva(raiz);
+}
+template <typename T>
+Lista Arbin<T>::listarFronteraRecursiva(NodoArbin* raiz, Lista frontera){
+    if (raiz->hijoder == NULL &&
+        raiz->hijoizq == NULL){
+        frontera.agregarPrincipio(raiz->campo);
+        return frontera;
+    }
+
+    if (raiz->hijoizq != NULL)
+        frontera = listarFronteraRecursiva(raiz->hijoizq, frontera);
+    if (raiz->hijoder != NULL)
+        frontera = listarFronteraRecursiva(raiz->hijoder, frontera);
+    return frontera;
+}
+template <typename T>
+Lista Arbin<T>::listarFrontera (){
+    Lista frontera = new Lista;
+    return listarFronteraRecursiva(raiz, frontera);
+ }
+
+
+ // Lista los nodos del arbol en preorder
+ template <typename T>
+void Arbin<T>::listarRecursivo(NodoArbin* raiz, Lista& lista){
+    if (raiz == NULL)
+        return;
+
+    lista.agregarFin(raiz->campo);
+    listarRecursivo(raiz->hijoizq, lista);
+    listarRecursivo(raiz->hijoder, lista);
+}
+template <typename T>
+Lista Arbin<T>::listarArbin(){
+    Lista listado = new Lista;
+    listarRecursivo(raiz, listado);
+    return listado;
+}
+
+template <typename T>
+void Arbin<T>::destructorRecursivo(NodoArbin* raiz){
+    if (raiz == NULL){
+        return;
+    }
+    destructorRecursivo(raiz->hijoizq);
+    destructorRecursivo(raiz->hijoder);
+    delete raiz;
+}
+template <typename T>
+Arbin<T>::~Arbin()
 {
-    //dtor
+    destructorRecursivo(raiz);
+    raiz = NULL;
 }
